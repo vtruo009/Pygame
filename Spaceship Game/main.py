3f605190a68c6c42aefe9c725c11294e5b524049
Van Truong <van.t414@gmail.com>
2021-10-16 23:16:57 -0700

    Added movement to both spaceships

--- Spaceship Game/main.py
+++ Spaceship Game/main.py
@@ -4,7 +4,7 @@ pygame.init()
 
 x = 50
 y = 50
-vel = 5
+VEL = 5
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("First Game")
@@ -15,13 +15,13 @@ SPACESHIP_WIDTH, SPACESHIP_HEIGHT = (55, 40)
 
 # Load spaceship
 YELLOW_SPACESHIP_IMG = pygame.image.load(
-    os.path.join('Assets', 'spaceship_yellow.png'))
+    os.path.join('Spaceship Game/Assets', 'spaceship_yellow.png'))
 # Scale spaceship
 YELLOW_SPACESHIP_IMG = pygame.transform.rotate(
     pygame.transform.scale(YELLOW_SPACESHIP_IMG, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
 
 RED_SPACESHIP_IMG = pygame.image.load(
-    os.path.join('Assets', 'spaceship_red.png'))
+    os.path.join('Spaceship Game/Assets', 'spaceship_red.png'))
 RED_SPACESHIP_IMG = pygame.transform.rotate(
     pygame.transform.scale(RED_SPACESHIP_IMG, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
 
@@ -30,6 +30,26 @@ def draw_window(red, yellow):
     WIN.blit(YELLOW_SPACESHIP_IMG, (yellow.x, yellow.y)) # use blit to draw text/images on the screen
     WIN.blit(RED_SPACESHIP_IMG, (red.x, red.y))
     pygame.display.update()
+
+def handle_yellow_movement(keys_pressed, yellow):
+    if keys_pressed[pygame.K_a]: # Left
+            yellow.x -= VEL
+    if keys_pressed[pygame.K_d]: # Right
+        yellow.x += VEL
+    if keys_pressed[pygame.K_w]: # Up
+        yellow.y -= VEL
+    if keys_pressed[pygame.K_s]: # Down
+        yellow.y += VEL
+
+def handle_red_movement(keys_pressed, red):
+    if keys_pressed[pygame.K_LEFT]: # Left
+            red.x -= VEL
+    if keys_pressed[pygame.K_RIGHT]: # Right
+        red.x += VEL
+    if keys_pressed[pygame.K_UP]: # Up
+        red.y -= VEL
+    if keys_pressed[pygame.K_DOWN]: # Down
+        red.y += VEL
 
 def main():
     # Rect(x, y, width, height)
@@ -43,7 +63,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        red.x -= 1
+        
+        keys_pressed = pygame.key.get_pressed()
+        handle_yellow_movement(keys_pressed, yellow)
+        handle_red_movement(keys_pressed, red)
+        
         draw_window(red, yellow)
 
     pygame.quit()
