e76f0d3cd93612cb440357aa6e70780f70846c30
Van Truong <van.t414@gmail.com>
2021-10-17 13:40:05 -0700

    Bullet keys configured

--- SpaceshipGame/main.py
+++ SpaceshipGame/main.py
@@ -5,6 +5,8 @@ pygame.init()
 x = 50
 y = 50
 VEL = 5
+BULLET_VEL = 7
+MAX_NUM_BULLETS = 3
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("First Game")
@@ -17,13 +19,13 @@ BORDER = pygame.Rect(WIDTH/2 - 5, 0, 10, HEIGHT)
 
 # Load spaceship
 YELLOW_SPACESHIP_IMG = pygame.image.load(
-    os.path.join('Spaceship Game/Assets', 'spaceship_yellow.png'))
+    os.path.join('SpaceshipGame/Assets', 'spaceship_yellow.png'))
 # Scale spaceship
 YELLOW_SPACESHIP_IMG = pygame.transform.rotate(
     pygame.transform.scale(YELLOW_SPACESHIP_IMG, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
 
 RED_SPACESHIP_IMG = pygame.image.load(
-    os.path.join('Spaceship Game/Assets', 'spaceship_red.png'))
+    os.path.join('SpaceshipGame/Assets', 'spaceship_red.png'))
 RED_SPACESHIP_IMG = pygame.transform.rotate(
     pygame.transform.scale(RED_SPACESHIP_IMG, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
 
@@ -59,6 +61,9 @@ def main():
     red = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
     yellow = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
 
+    yellow_bullets = []
+    red_bullets = []
+
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -66,6 +71,14 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q and len(yellow_bullets) < MAX_NUM_BULLETS:
+                    bullet = pygame.Rect(yellow.x + yellow.width, yellow.y + yellow.height/2 - 2, 10, 5)
+                    yellow_bullets.append(bullet)
+                if event.key == pygame.K_SPACE and len(red_bullets) < MAX_NUM_BULLETS:
+                    bullet = pygame.Rect(red.x, red.y + red.height/2 - 2, 10, 5)
+                    red_bullets.append(bullet)
+        
         
         keys_pressed = pygame.key.get_pressed()
         handle_yellow_movement(keys_pressed, yellow)
